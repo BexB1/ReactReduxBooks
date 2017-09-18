aea7e6e876c4fa264466944cc2d7b077bba564b0
Bex Bolton <bexb1@hotmail.com>
2017-09-18 18:13:16 +0100

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'e7afa1c94a9121e4e9bbd02b59f86b2a';
+const API_KEY = '';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
